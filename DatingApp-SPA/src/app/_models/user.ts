36102b2fd8647cbee8763028138814c41d10fd23
unknown <tlewis2171@gmail.com>
2018-10-07 12:49:10 -0400

    End of Section 9

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,21 @@
+import { Photo } from './photo';
+
+// replicate what we are returning for our user
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    // properties for UserForDetailDto
+    // optional come after required properties
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+// removed httpOptions since we added to app.module.ts
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Authorization': 'Bearer ' + localStorage.getItem('token')
+//   })
+// };
+
+@Injectable({
+  providedIn: 'root'
+})
+  export class UserService {
+    baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+  // return error
+  // return this.http.get(this.baseUrl + 'users');
+  // fix with:
+   // return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl + 'users');
+  }
+
+  getUser(id): Observable<User> {
+    // return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+
+
+}
